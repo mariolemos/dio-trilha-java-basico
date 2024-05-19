49fa8e3d33da3100e84e0127f47df2803612e801
mariolemos <lemosmm64@gmail.com>
2024-05-19 13:48:03 -0300

    Projeto Rodando

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Digite nome do Cliente: ");
+        String nomeConta = scanner.nextLine();
+
+        System.out.print("Digite o Saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.print("Digite sua Conta; ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Nome do Cliente: " + nomeConta);
+        System.out.println("Conta número: " + conta);
+        System.out.println("Agência número: " + agencia);
+        System.out.println("Saldo: " + saldo);
+
+    }
 }
